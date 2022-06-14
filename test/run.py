54e9ff1642677aa2eeafc428b34f93366a3c3cac
郑春兴 <zhengchunxing@aegis-data.cn>
2022-06-14 18:24:54 +0800

    增加运行主入口

---
+++ test/run.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+# @Time : 2022/6/14 16:55
+# @Author : 郑春兴
+# @Email : zhengchunxing@aegis-data.cn
+# @File : run.py
+# @Project : PlaywrightProject
+import pytest
+
+if __name__ == "__main__":
+     pytest.main(['-s', '-v', '-q', '--alluredir', './allure_result'])
