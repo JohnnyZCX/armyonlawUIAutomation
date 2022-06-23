91c0f0d9b8bc27d2a0a4a02792f007541cd0a837
郑春兴 <zhengchunxing@aegis-data.cn>
2022-06-23 18:05:29 +0800

    修改机器人日志测试用例

--- pages/robotpageconfig.py
+++ pages/robotpageconfig.py
@@ -5,6 +5,8 @@
 # @Email : zhengchunxing@aegis-data.cn
 # @File : robotpageconfig.py
 # @Project : PlaywrightProject
+import random
+
 from playwright.async_api import Page
 
 
@@ -17,6 +19,8 @@ class RobotPageConfig:
         self.big_screen_config_tab = self.page.locator("//div[@role=\"tablist\"]/div[text()=\"大屏配置\"]")
         # 大标题配置tab
         self.headline_config_tab = self.page.locator("//div[@role=\"tablist\"]/div[text()=\"大标题配置\"]")
+        # 大标题配置tab中输入框
+        self.headline_config_tab_input = self.page.locator("//span[text()=\"大标题配置\"]/ancestor::div[@class=\"index-module_header_2PLLL\"]/following-sibling::div[@class=\"index-module_container_Uuzng\"]//input")
         # 热门问题配置tab
         self.popular_questions_config_tab = self.page.locator("//div[@role=\"tablist\"]/div[text()=\"热门问题配置\"]")
         # 功能模块配置tab
@@ -28,7 +32,7 @@ class RobotPageConfig:
         # 提交并同步按钮
         self.commit_sync_button = self.page.locator("button:has-text(\"提交并同步\")")
         # 背景配置中的待机背景按钮
-        self.standby_background_button = self.page.locator("//span[@class=\"index-module_activeBtn_1g9j1 index-module_backgroundBtn_1KpUy\"]")
+        self.standby_background_button = self.page.locator("//span[@class=\"index-module_backgroundBtn_1KpUy\"]")
         # 机器人配置提交同步成功文本校验  //div[@class="deleteWarning"],机器人配置提交同步成功
         # 机器人配置同步成功提示框中的确定按钮
         self.commit_success_confirm_button = self.page.locator("//div[@class=\"index-module_bottomSubmit_mTa1n\"]/following-sibling::div//button")
@@ -36,13 +40,27 @@ class RobotPageConfig:
         self.background_config_set_default_button = self.page.locator("//span[@class=\"index-module_backgroundBtn_1KpUy\"]/ancestor::div[@class=\"index-module_header_2PLLL\"]/following-sibling::div//span[text()=\'使用默认\']")
         # 背景配置tab中上传背景图input标签
         self.upload_background_image = self.page.locator("//span[@class=\"index-module_backgroundBtn_1KpUy\"]/ancestor::div[@class=\"index-module_header_2PLLL\"]/following-sibling::div//input[@type=\'file\']")
-
+        # 大屏配置tab中第二张图片
+        self.big_screen_config_second_image = self.page.locator("//div[@class=\"swiper-wrapper\"]/div[2]/img")
+        # 大屏配置tab中上传图片input标签
+        self.upload_big_screen_image = self.page.locator("//div[@class=\"swiper-wrapper\"]//input[@type=\"file\"]")
+        # 大屏配置tab中第三张图片的删除按钮
+        self.big_screen_config_third_delete_button = self.page.locator("//div[@class=\"swiper-wrapper\"]/div[@class=\"swiper-slide\"]/img/following-sibling::span[@class=\"index-module_deleteIcon_37FBd\"]")
+        # 是否确认删除提示框中确定按钮
+        self.delete_image_dialog_confirm_button = self.page.locator("//div[@class=\"deleteWarning\"][text()=\"是否确认删除？\"]/ancestor::div[@class=\"el-dialog__body\"]/following-sibling::div[@class=\"el-dialog__footer\"]//button[1]")
 
 
 
     def assertText(self,element,text_value):
         text = self.page.inner_text(element)
         assert text ==  text_value
+
+    def GBK2312(self):
+        head = random.randint(0xb0, 0xf7)
+        body = random.randint(0xa1, 0xfe)
+        val = f'{head:x}{body:x}'
+        str_join = bytes.fromhex(val).decode('gb2312')
+        return str_join
 
 
 
--- test/test_robot_page_config.py
+++ test/test_robot_page_config.py
@@ -44,12 +44,12 @@ class TestRobotPageConfig:
             time.sleep(2)
             robotPageConfig.commit_sync_button.click()
             time.sleep(1)
-            robotPageConfig.assertText("//div[@class=\"deleteWarning\"]","机器人配置提交同步成功")
+            robotPageConfig.assertText("//div[@role=\"progressbar\"]/preceding-sibling::div[@class=\"deleteWarning\"]","机器人配置提交同步成功")
             robotPageConfig.commit_success_confirm_button.click()
             robotPageConfig.background_config_set_default_button.click()
             robotPageConfig.commit_sync_button.click()
             time.sleep(1)
-            robotPageConfig.assertText("//div[@class=\"deleteWarning\"]", "机器人配置提交同步成功")
+            robotPageConfig.assertText("//div[@role=\"progressbar\"]/preceding-sibling::div[@class=\"deleteWarning\"]", "机器人配置提交同步成功")
             robotPageConfig.commit_success_confirm_button.click()
             test_log.info("机器人首页背景配置测试通过")
         except Exception as e:
@@ -58,6 +58,119 @@ class TestRobotPageConfig:
             test_log.debug('预期结果：首页背景更改和使用默认背景都能正常提交并同步成功')
             test_log.exception(e)
             pytest.fail("预期结果：首页背景更改和使用默认背景都能正常提交并同步成功")
+
+    @allure.story("机器人待机背景配置")
+    def test_background_config_standby_page(self,page,superUserAmdin_login):
+        userManagementPage = superUserAmdin_login
+        userManagementPage.equipment_manage_menu.click()
+        equipmentManagePage = EquipmentManage(page)
+        equipmentManagePage.robot_number_input.wait_for()
+        equipmentManagePage.robot_number_input.fill("D4A0D010020534M1D5")
+        equipmentManagePage.search_button.click()
+        time.sleep(1)
+        equipmentManagePage.first_row_page_config_button.click()
+        try:
+            time.sleep(2)
+            robotPageConfig = RobotPageConfig(page)
+            robotPageConfig.background_config_tab.wait_for()
+            time.sleep(1)
+            robotPageConfig.standby_background_button.click()
+            robotPageConfig.upload_background_image.set_input_files("test\\background.png")
+            time.sleep(2)
+            robotPageConfig.commit_sync_button.click()
+            time.sleep(1)
+            robotPageConfig.assertText("//div[@role=\"progressbar\"]/preceding-sibling::div[@class=\"deleteWarning\"]", "机器人配置提交同步成功")
+            robotPageConfig.commit_success_confirm_button.click()
+            robotPageConfig.background_config_set_default_button.click()
+            robotPageConfig.commit_sync_button.click()
+            time.sleep(1)
+            robotPageConfig.assertText("//div[@role=\"progressbar\"]/preceding-sibling::div[@class=\"deleteWarning\"]", "机器人配置提交同步成功")
+            robotPageConfig.commit_success_confirm_button.click()
+            test_log.info("机器人待机背景配置测试通过")
+        except Exception as e:
+            allure.attach(page.screenshot(), "用例失败截图", allure.attachment_type.PNG)
+            test_log.error('机器人待机背景配置测试不通过')
+            test_log.debug('预期结果：待机背景更改和使用默认背景都能正常提交并同步成功')
+            test_log.exception(e)
+            pytest.fail("预期结果：待机背景更改和使用默认背景都能正常提交并同步成功")
+
+    @allure.story("机器人大屏配置测试")
+    def test_big_screen_config(self,page,superUserAmdin_login):
+        userManagementPage = superUserAmdin_login
+        userManagementPage.equipment_manage_menu.click()
+        equipmentManagePage = EquipmentManage(page)
+        equipmentManagePage.robot_number_input.wait_for()
+        equipmentManagePage.robot_number_input.fill("D4A0D010020534M1D5")
+        equipmentManagePage.search_button.click()
+        time.sleep(1)
+        equipmentManagePage.first_row_page_config_button.click()
+        try:
+            time.sleep(2)
+            robotPageConfig = RobotPageConfig(page)
+            robotPageConfig.background_config_tab.wait_for()
+            robotPageConfig.big_screen_config_tab.click()
+            robotPageConfig.upload_big_screen_image.set_input_files("test\\background.png")
+            time.sleep(2)
+            robotPageConfig.big_screen_config_third_delete_button.click()
+            robotPageConfig.delete_image_dialog_confirm_button.click()
+            time.sleep(1)
+            test_log.info("上传图片并删除图片测试通过")
+            robotPageConfig.commit_sync_button.click()
+            time.sleep(1)
+            robotPageConfig.assertText("//div[@role=\"progressbar\"]/preceding-sibling::div[@class=\"deleteWarning\"]", "机器人配置提交同步成功")
+            robotPageConfig.commit_success_confirm_button.click()
+            test_log.info("更改大屏背景设置测试通过")
+        except Exception as e:
+            allure.attach(page.screenshot(), "用例失败截图", allure.attachment_type.PNG)
+            test_log.error('机器人大屏配置测试不通过')
+            test_log.debug('预期结果：上传图片并删除图片测试通过，更改大屏背景设置测试通过')
+            test_log.exception(e)
+            pytest.fail("预期结果：上传图片并删除图片测试通过，更改大屏背景设置测试通过")
+
+    @allure.story("机器人大标题配置测试")
+    def test_headline_config(self,page,superUserAmdin_login):
+        userManagementPage = superUserAmdin_login
+        userManagementPage.equipment_manage_menu.click()
+        equipmentManagePage = EquipmentManage(page)
+        equipmentManagePage.robot_number_input.wait_for()
+        equipmentManagePage.robot_number_input.fill("D4A0D010020534M1D5")
+        equipmentManagePage.search_button.click()
+        time.sleep(1)
+        equipmentManagePage.first_row_page_config_button.click()
+        try:
+            time.sleep(2)
+            robotPageConfig = RobotPageConfig(page)
+            robotPageConfig.background_config_tab.wait_for()
+            robotPageConfig.headline_config_tab.click()
+            robotPageConfig.headline_config_tab_input.fill("请对我说："+robotPageConfig.GBK2312()+robotPageConfig.GBK2312()+robotPageConfig.GBK2312()+robotPageConfig.GBK2312()+robotPageConfig.GBK2312()+robotPageConfig.GBK2312())
+            robotPageConfig.page.locator("//span[text()=\"大标题配置\"]").click()
+            robotPageConfig.commit_sync_button.click()
+            time.sleep(1)
+            robotPageConfig.assertText("//div[@role=\"progressbar\"]/preceding-sibling::div[@class=\"deleteWarning\"]","机器人配置提交同步成功")
+            robotPageConfig.commit_success_confirm_button.click()
+            test_log.info("机器人大标题配置测试通过")
+        except Exception as e:
+            allure.attach(page.screenshot(), "用例失败截图", allure.attachment_type.PNG)
+            test_log.error('机器人大标题配置测试不通过')
+            test_log.debug('预期结果：修改大标题文本内容并提交同步测试通过')
+            test_log.exception(e)
+            pytest.fail("预期结果：修改大标题文本内容并提交同步测试通过")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
