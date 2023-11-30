80e6db6356a4f8657eb203c2ccb2c2c677d5a2f2
Johnny Zheng <42194910+JohnnyZCX@users.noreply.github.com>
2023-11-30 15:16:55 +0800

    Update run.py
    修改了运行测试用例的模式，通过增加改变一些参数

--- test/run.py
+++ test/run.py
@@ -5,10 +5,18 @@
 # @Email : zhengchunxing@aegis-data.cn
 # @File : run.py
 # @Project : PlaywrightProject
+# encoding=utf-8
 import os
-from subprocess import Popen, PIPE, STDOUT
 import pytest
 
 if __name__ == "__main__":
-     pytest.main(['-s', '-v', '--alluredir', 'allure_result'])
-     #os.system('pytest -sq --alluredir=../allure_result')
+    # 收集并运行测试用例，同时以json格式文件生成测试结果数据且覆盖之前的结果数据，--reruns=4代表失败重跑4次，--headed代表有头浏览器模式
+    pytest.main(['--headed', '--screenshot=on', '--video=on', '-sv', '--reruns=4', '--reruns-delay=3', '--alluredir', 'allure_result', '--clean-alluredir'])
+    # 下面命令开启无头模式，--screenshot=on和--video=on代表开启截图和录屏功能
+    """pytest.main(
+        ['--screenshot=on', '--video=on', '-sv', '--reruns=4', '--reruns-delay=2', '--alluredir', 'allure_result',
+         '--clean-alluredir'])"""
+    # 生成测试报告并清空历史报告的数据
+    os.system(r"allure generate allure_result -c -o allure-report")
+    # 打开浏览器并展示报告
+    os.system(r"allure open allure-report")
